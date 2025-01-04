c37686a73f4a9bbfa7c311f7f2cf8793c646a309
Keshav-Maheshwari <maheshwarikeshav029@gmail.com>
2025-01-04 16:13:08 +0530

    added roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,43 +1,47 @@
 import User from "../models/userModel.js";
 
-
 export const registerUser = async (req, res) => {
-    const { name, email, password, role } = req.body;
+  const { name, email, password } = req.body;
 
-    try {
-        const userExists = await User.findOne({ email });
+  try {
+    const userExists = await User.findOne({ email });
 
-        if (userExists) {
-            return res.status(400).json({ msg: "User already exists" });
-        }
-
-        const newUser = new User({ name, email, password, role });
-
-        await newUser.save();
-
-        res.json(newUser);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server error");
+    if (userExists) {
+      return res.status(400).json({ msg: "User already exists" });
     }
-}
+    let role = "user";
+    if (email === "admin@gmail.com" && password === "admin123") {
+      role = "admin";
+    }
+
+    const newUser = new User({ name, email, password, role });
+
+    await newUser.save();
+
+    res.json(newUser);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+};
 
 export const loginUser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(400).json({ msg: "User not found" });
-        }
-
-        // Directly compare the plaintext passwords
-        if (user.password !== password) return res.status(400).json({ msg: "Invalid password" });
-
-        res.json(user);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server error");
+    if (!user) {
+      return res.status(400).json({ msg: "User not found" });
     }
-}
+
+    // Directly compare the plaintext passwords
+    if (user.password !== password)
+      return res.status(400).json({ msg: "Invalid password" });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["admin", "employee"],
+      enum: ["admin", "govt","user"],
       default: "employee",
     },
   },
